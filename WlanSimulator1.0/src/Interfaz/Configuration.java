b6adeaec8b4dcb8c62d3373e0a053c423c7aa028
Cesar Serrano Lopez <cesarserranolopez@gmail.com>
2012-11-23 19:40:13 +0100

    Revert "WifiSim1.0"
    This reverts commit dc88324aa71fe61cf9be31983607d303a680b00d.

--- WlanSimulator1.0/src/Interfaz/Configuration.java
+++
@@ -1,9 +0,0 @@
-package Interfaz;
-
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-public class Configuration extends JPanel{
-	
-	public static JTextField jTextField_energy = null;
-}
